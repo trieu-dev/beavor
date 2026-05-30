6d4e9a64f9ed326c11d4ad476f59e07ee5dcc389
trieu-dev <trieula.work@gmail.com>
2026-05-30 16:42:27 +0700

    refactor: replace BillModel with Expense, Member, Settlement models

---
+++ lib/models/expense.dart
@@ -0,0 +1,45 @@
+class Expense {
+  final String id;
+  String payerId;
+  final String title;
+  double amount;
+  final List<String> participantIds; // who shares this expense
+
+  Expense({
+    required this.id,
+    required this.payerId,
+    required this.title,
+    required this.amount,
+    required this.participantIds,
+  });
+
+  factory Expense.def() =>
+      Expense(id: "", payerId: '', amount: 0, participantIds: [], title: '');
+
+  double get sharePerPerson => amount / participantIds.length;
+
+  @override
+  String toString() => '$payerId → $title - $amount - ${participantIds.join(', ')}';
+
+  factory Expense.fromMap(Map<String, dynamic> map) {
+    return Expense(
+      id: map['id'],
+      title: map['title'],
+      amount: (map['amount'] as num).toDouble(),
+      participantIds: map['participantIds'] != null
+          ? map['participantIds'].map((x) => x).toList()
+          : [],
+      payerId: map['payerId'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'participantIds': participantIds,
+      'payerId': payerId,
+    };
+  }
+}
